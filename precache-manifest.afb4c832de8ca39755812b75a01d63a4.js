2bbd47c33c06ec9ccfee19d64d96f6d86883fb63
Sahar Bitton <saharbi@walla.co.il>
2020-08-08 12:24:23 +0300

    Updates

---
+++ precache-manifest.afb4c832de8ca39755812b75a01d63a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada5736f9b55ad3c44b85f7486952a35",
+    "url": "/wargame/index.html"
+  },
+  {
+    "revision": "83d4a203ea7450556be5",
+    "url": "/wargame/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d3a2a0a7679d5b10ebd4",
+    "url": "/wargame/static/css/main.e251d4b4.chunk.css"
+  },
+  {
+    "revision": "83d4a203ea7450556be5",
+    "url": "/wargame/static/js/2.956aceb3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wargame/static/js/2.956aceb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a2a0a7679d5b10ebd4",
+    "url": "/wargame/static/js/main.2a007054.chunk.js"
+  },
+  {
+    "revision": "a5fccbc14bb30c8461b2",
+    "url": "/wargame/static/js/runtime-main.6de7e5aa.js"
+  }
+]);
